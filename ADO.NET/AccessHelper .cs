a7f0575c00e1f4302c03221ec3f24943e3ecc074
Sung <sung.liu@coscomms.com>
2020-11-27 16:33:36 +0800

    Add AccessHelper and Rename file

--- ADO.NET/AccessHelper .cs
+++ ADO.NET/AccessHelper .cs
@@ -1,18 +1,15 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.OleDb;
 
 namespace ADO.NET
 {
-    public class MySQLHelper
+    public class AccessHelper
     {
-        /// <summary>
-        /// DataBase connection string example: "Data Source=ip;Initial Catalog=DataBase;Persist Security Info=True;User ID=id;Password=pw"
-        /// </summary>
         private string DBConnectString { get; set; }
 
-        public MySQLHelper(string connectString)
+        public AccessHelper(string connectString)
         {
             DBConnectString = connectString;
         }
@@ -23,17 +20,17 @@ namespace ADO.NET
         /// <param name="commandString">SQL command string</param>
         /// <param name="parameters">Parameters in commanc string</param>
         /// <returns>DataTable</returns>
-        public DataTable ExecSQL(string commandString, IEnumerable<MySqlParameter> parameters = null)
+        public DataTable ExecSQL(string commandString, IEnumerable<OleDbParameter> parameters = null)
         {
-            MySqlConnection conn = new MySqlConnection(DBConnectString);
-            MySqlCommand cmd = new MySqlCommand(commandString, conn)
+            OleDbConnection conn = new OleDbConnection(DBConnectString);
+            OleDbCommand cmd = new OleDbCommand(commandString, conn)
             {
                 CommandType = CommandType.Text
             };
 
             if (parameters != null)
             {
-                foreach (MySqlParameter x in parameters)
+                foreach (OleDbParameter x in parameters)
                 {
                     if ((x.Direction == ParameterDirection.InputOutput || x.Direction == ParameterDirection.Input) && x.Value == null)
                     {
@@ -50,7 +47,7 @@ namespace ADO.NET
                 //Open the connection
                 conn.Open();
                 //Load Data by each row
-                MySqlDataReader dr = cmd.ExecuteReader();
+                OleDbDataReader dr = cmd.ExecuteReader();
                 //Stored in data table
                 dt.Load(dr);
                 //Release cmd memories
@@ -75,18 +72,18 @@ namespace ADO.NET
         /// <param name="commandString">SQL command string</param>
         /// <param name="parameters">Parameters in command string</param>
         /// <returns>Affected rows</returns>
-        public int GetAffectedRows(string commandString, IEnumerable<MySqlParameter> parameters = null)
+        public int GetAffectedRows(string commandString, IEnumerable<OleDbParameter> parameters = null)
         {
             int affectedRows = 0;
-            MySqlConnection conn = new MySqlConnection(DBConnectString);
-            MySqlCommand cmd = new MySqlCommand(commandString, conn)
+            OleDbConnection conn = new OleDbConnection(DBConnectString);
+            OleDbCommand cmd = new OleDbCommand(commandString, conn)
             {
                 CommandType = CommandType.Text
             };
 
             if (parameters != null)
             {
-                foreach (MySqlParameter x in parameters)
+                foreach (OleDbParameter x in parameters)
                 {
                     if ((x.Direction == ParameterDirection.InputOutput || x.Direction == ParameterDirection.Input) && x.Value == null)
                     {
@@ -125,17 +122,17 @@ namespace ADO.NET
         /// <param name="procudure">The name of stored procedure</param>
         /// <param name="parameters">Parameters that stored procedure required</param>
         /// <returns>DataTable</returns>
-        public DataTable ExecProc(string procudure, IEnumerable<MySqlParameter> parameters = null)
+        public DataTable ExecProc(string procudure, IEnumerable<OleDbParameter> parameters = null)
         {
-            MySqlConnection conn = new MySqlConnection(DBConnectString);
-            MySqlCommand cmd = new MySqlCommand(procudure, conn)
+            OleDbConnection conn = new OleDbConnection(DBConnectString);
+            OleDbCommand cmd = new OleDbCommand(procudure, conn)
             {
                 CommandType = CommandType.StoredProcedure
             };
 
             if (parameters != null)
             {
-                foreach (MySqlParameter x in parameters)
+                foreach (OleDbParameter x in parameters)
                 {
                     if ((x.Direction == ParameterDirection.InputOutput || x.Direction == ParameterDirection.Input) && x.Value == null)
                     {
@@ -152,7 +149,7 @@ namespace ADO.NET
                 //Open the connection
                 conn.Open();
                 //Load Data by each row
-                MySqlDataReader dr = cmd.ExecuteReader();
+                OleDbDataReader dr = cmd.ExecuteReader();
                 //Stored in data table
                 dt.Load(dr);
                 //Release cmd memories
@@ -197,7 +194,7 @@ namespace ADO.NET
             return model;
         }
 
-        public IEnumerable<T> ExecSQL<T>(string commandString, IEnumerable<MySqlParameter> parameters = null) where T : class, new()
+        public List<T> ExecSQL<T>(string commandString, IEnumerable<OleDbParameter> parameters = null) where T : class, new()
         {
             DataTable dt = ExecSQL(commandString, parameters);
 
@@ -215,7 +212,7 @@ namespace ADO.NET
             return list;
         }
 
-        public IEnumerable<T> ExecPro<T>(string procedure, IEnumerable<MySqlParameter> parameters = null) where T : class, new()
+        public List<T> ExecPro<T>(string procedure, IEnumerable<OleDbParameter> parameters = null) where T : class, new()
         {
             DataTable dt = ExecProc(procedure, parameters);
 
---
+++ ADO.NET/MySQLHelper.cs
@@ -0,0 +1,234 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ADO.NET
+{
+    public class MySQLHelper
+    {
+        private string DBConnectString { get; set; }
+
+        public MySQLHelper(string connectString)
+        {
+            DBConnectString = connectString;
+        }
+
+        /// <summary>
+        /// Execute SQL command string
+        /// </summary>
+        /// <param name="commandString">SQL command string</param>
+        /// <param name="parameters">Parameters in commanc string</param>
+        /// <returns>DataTable</returns>
+        public DataTable ExecSQL(string commandString, IEnumerable<MySqlParameter> parameters = null)
+        {
+            MySqlConnection conn = new MySqlConnection(DBConnectString);
+            MySqlCommand cmd = new MySqlCommand(commandString, conn)
+            {
+                CommandType = CommandType.Text
+            };
+
+            if (parameters != null)
+            {
+                foreach (MySqlParameter x in parameters)
+                {
+                    if ((x.Direction == ParameterDirection.InputOutput || x.Direction == ParameterDirection.Input) && x.Value == null)
+                    {
+                        x.Value = DBNull.Value;
+                    }
+
+                    cmd.Parameters.Add(x);
+                }
+            }
+
+            DataTable dt = new DataTable();
+            try
+            {
+                //Open the connection
+                conn.Open();
+                //Load Data by each row
+                MySqlDataReader dr = cmd.ExecuteReader();
+                //Stored in data table
+                dt.Load(dr);
+                //Release cmd memories
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //Close the connection
+                conn.Close();
+            }
+
+            return dt;
+        }
+
+        /// <summary>
+        /// Execute SQL command string
+        /// </summary>
+        /// <param name="commandString">SQL command string</param>
+        /// <param name="parameters">Parameters in command string</param>
+        /// <returns>Affected rows</returns>
+        public int GetAffectedRows(string commandString, IEnumerable<MySqlParameter> parameters = null)
+        {
+            int affectedRows = 0;
+            MySqlConnection conn = new MySqlConnection(DBConnectString);
+            MySqlCommand cmd = new MySqlCommand(commandString, conn)
+            {
+                CommandType = CommandType.Text
+            };
+
+            if (parameters != null)
+            {
+                foreach (MySqlParameter x in parameters)
+                {
+                    if ((x.Direction == ParameterDirection.InputOutput || x.Direction == ParameterDirection.Input) && x.Value == null)
+                    {
+                        x.Value = DBNull.Value;
+                    }
+
+                    cmd.Parameters.Add(x);
+                }
+            }
+
+            try
+            {
+                //Open the connection
+                conn.Open();
+                //Execute
+                affectedRows = cmd.ExecuteNonQuery();
+                //Release cmd memories
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //Close the connection
+                conn.Close();
+            }
+
+            return affectedRows;
+        }
+
+        /// <summary>
+        /// Execute stored procedure
+        /// </summary>
+        /// <param name="procudure">The name of stored procedure</param>
+        /// <param name="parameters">Parameters that stored procedure required</param>
+        /// <returns>DataTable</returns>
+        public DataTable ExecProc(string procudure, IEnumerable<MySqlParameter> parameters = null)
+        {
+            MySqlConnection conn = new MySqlConnection(DBConnectString);
+            MySqlCommand cmd = new MySqlCommand(procudure, conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            if (parameters != null)
+            {
+                foreach (MySqlParameter x in parameters)
+                {
+                    if ((x.Direction == ParameterDirection.InputOutput || x.Direction == ParameterDirection.Input) && x.Value == null)
+                    {
+                        x.Value = DBNull.Value;
+                    }
+
+                    cmd.Parameters.Add(x);
+                }
+            }
+
+            DataTable dt = new DataTable();
+            try
+            {
+                //Open the connection
+                conn.Open();
+                //Load Data by each row
+                MySqlDataReader dr = cmd.ExecuteReader();
+                //Stored in data table
+                dt.Load(dr);
+                //Release cmd memories
+                cmd.Dispose();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //Close the connection
+                conn.Close();
+            }
+
+            return dt;
+        }
+
+        private T Mapping<T>(DataTable dt, DataRow dr) where T : class, new()
+        {
+            T model = new T();
+            foreach (var prop in model.GetType().GetProperties())
+            {
+                if (!prop.CanWrite)
+                {
+                    continue;
+                }
+
+                if (dt.Columns.Contains(prop.Name))
+                {
+                    object propValue = null;
+                    var value = dr[prop.Name];
+                    if (value != DBNull.Value)
+                    {
+                        propValue = value;
+                    }
+
+                    prop.SetValue(model, propValue, null);
+                }
+            }
+
+            return model;
+        }
+
+        public IEnumerable<T> ExecSQL<T>(string commandString, IEnumerable<MySqlParameter> parameters = null) where T : class, new()
+        {
+            DataTable dt = ExecSQL(commandString, parameters);
+
+            if (dt == null)
+            {
+                throw new ArgumentNullException("DataTable is null!");
+            }
+
+            List<T> list = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T model = Mapping<T>(dt, row);
+                list.Add(model);
+            }
+            return list;
+        }
+
+        public IEnumerable<T> ExecPro<T>(string procedure, IEnumerable<MySqlParameter> parameters = null) where T : class, new()
+        {
+            DataTable dt = ExecProc(procedure, parameters);
+
+            if (dt == null)
+            {
+                throw new ArgumentNullException("DataTable is null!");
+            }
+
+            List<T> list = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T model = Mapping<T>(dt, row);
+                list.Add(model);
+            }
+
+            return list;
+        }
+    }
+}
