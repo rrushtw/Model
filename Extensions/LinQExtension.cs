78a01dba7673f95d95f79274399563eebccc388b
Sung <sung.liu@coscomms.com>
2020-12-11 18:58:35 +0800

    Add IsEqual and Distinct extension

---
+++ Extensions/LinQExtension.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class LinQExtension
+    {
+        public static IEnumerable<T> DisTinct<T>(this IEnumerable<T> list)
+        {
+            List<T> resultList = new List<T>();
+            List<string> stringList = new List<string>();
+
+            foreach (T model in list)
+            {
+                string jsonString = JsonConvert.SerializeObject(model);
+
+                if (stringList.Contains(jsonString)) continue;
+
+                stringList.Add(jsonString);
+                resultList.Add(model);
+            }
+
+            return resultList;
+        }
+    }
+}
